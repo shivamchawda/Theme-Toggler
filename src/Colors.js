c24d30e3827c29c7c64ac97b41f58c7486e30eac
Shivam <shivamchawda23@gmail.com>
2021-05-14 21:26:06 +0530

    initial commit

---
+++ src/Colors.js
@@ -0,0 +1,18 @@
+const AppTheme = {
+    light: {
+        textColor: "#000",
+        backgroundColor: "#fff",
+        border: "2px solid #000"
+    },
+    dark: {
+        textColor: "#000",
+        backgroundColor: "#333",
+        border: "2px dashed #000"
+    }
+};
+
+
+
+
+
+export default AppTheme;
